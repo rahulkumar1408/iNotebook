2310791d7736f4e2b0b9c8b1742e8b2b88fb06a8
Rahul kumar <rahulkumarsah269@gmail.com>
2024-05-14 10:26:37 +0530

    changes

--- Backend/db.js
+++ Backend/db.js
@@ -5,7 +5,7 @@ const mongoURI = process.env.mongoURI;
 
 const ConnectToMongo = async ()=>{
     try {
-        await mongoose.connect(mongoURI);
+        await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('Connected to MongoDB');
       } catch (error) {
         console.error('Error connecting to MongoDB:', error);
